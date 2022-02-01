ad5f82dc16e821b905ed04da157a5cf581cdcbc1
Amarjeet Malpotra <amalpotra@catalystone.com>
2022-02-02 00:25:57 +0530

    Working validations & UI revamps
    - Stick to requirements for UI
    - Debounce user inputs to avoid overhead
    - Simplified resetSelect mechanism
    - Removed unused bootstrap js
    - Customized email switch as per requirements
    
    Signed-off-by: Amarjeet Malpotra <amalpotra@catalystone.com>

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-countries = document.getElementById('country')
-states = document.getElementById('state')
-cities = document.getElementById('city')
-countryCodes = document.getElementById('country-code')
-email = document.getElementById('email')
-emailSwitch = document.getElementById('email-switch')
+const id = (id) => document.getElementById(id)
+
+const userName = id('userName')
+const firstName = id('firstName')
+const lastName = id('lastName')
+const countryCodes = id('countryCode')
+const phone = id('phone')
+const emailSwitch = id('emailSwitch')
+const email = id('email')
+const ageGroup = id('ageGroup')
+const desiredTeam = document.getElementsByName('desiredTeam')
+const desiredPosition = document.getElementsByName('desiredPosition')
+const address = id('address')
+const pincode = id('pincode')
+const countries = id('country')
+const states = id('state')
+const cities = id('city')
+const form = id('form')
 
 let countryData = []
 
+// Wait for DOM to load before fetching API
 document.addEventListener('DOMContentLoaded', () => {
 	fetch('https://countriesnow.space/api/v0.1/countries/codes')
 		.then((response) => response.json())
@@ -23,27 +36,30 @@ document.addEventListener('DOMContentLoaded', () => {
 						country.name
 					}`
 					optionCode.setAttribute('value', country.dial_code)
-					countryCodes.appendChild(optionCode)
+					countryCodes.append(optionCode)
 
 					optionCountry.innerText = country.name
-					countries.appendChild(optionCountry)
+					countries.append(optionCountry)
 				})
 
 			countryCodes.removeAttribute('disabled')
 			countries.removeAttribute('disabled')
 
-			// Setting +91 as default
+			// Setting +91 as default and carry usual event triggering
 			countryCodes.value = '+91'
 			countryCodes.dispatchEvent(new Event('change'))
 		})
 		.catch((error) => {
 			console.warn(`Stupid network error! - ${error.message}`)
 		})
+
+	// Focus firstName
+	firstName.focus()
 })
 
 countryCodes.addEventListener('change', () => {
 	countries.value = countryData.find(
-		(country) => country.dial_code == countryCodes.value
+		(country) => country.dial_code === countryCodes.value
 	).name
 	countries.dispatchEvent(new Event('change'))
 })
@@ -58,12 +74,12 @@ countries.addEventListener('change', () => {
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			resetSelect(states, 'country')
+			resetSelect(states)
 
 			data.data.states.map((state) => {
 				const option = document.createElement('option')
 				option.innerText = state.name
-				states.appendChild(option)
+				states.append(option)
 			})
 			states.removeAttribute('disabled')
 		})
@@ -72,7 +88,7 @@ countries.addEventListener('change', () => {
 		})
 		.finally(() => {
 			if (!cities.hasAttribute('disabled')) {
-				resetSelect(cities, 'country')
+				resetSelect(cities)
 				cities.setAttribute('disabled', '')
 			}
 		})
@@ -88,12 +104,12 @@ states.addEventListener('change', () => {
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			resetSelect(cities, 'state')
+			resetSelect(cities)
 
 			data.data.map((city) => {
 				const option = document.createElement('option')
 				option.innerText = city
-				cities.appendChild(option)
+				cities.append(option)
 			})
 			cities.removeAttribute('disabled')
 		})
@@ -102,24 +118,141 @@ states.addEventListener('change', () => {
 		})
 })
 
-const resetSelect = (node, from) => {
-	while (node.firstChild) {
-		node.firstChild.remove()
+const resetSelect = (node) => {
+	while (node.children.length > 1) {
+		node.lastChild.remove()
 	}
-
-	const option = document.createElement('option')
-	option.setAttribute('selected', '')
-	option.setAttribute('hidden', '')
-
-	if (node.id === 'city' && from === 'country')
-		option.innerText = `Select the state first`
-	else option.innerText = `Select the ${node.id} now`
-
-	node.appendChild(option)
 }
 
 emailSwitch.addEventListener('change', () => {
 	email.hasAttribute('disabled')
 		? email.removeAttribute('disabled')
 		: email.setAttribute('disabled', '')
+})
+
+// Debounce user inputs
+const debounce = (func, timeout = 500) => {
+	let timer
+	return (...args) => {
+		clearTimeout(timer)
+		timer = setTimeout(() => {
+			func.apply(this, args)
+		}, timeout)
+	}
+}
+
+// Validation functions
+const checkUsername = () => {
+	// Some db magic
+}
+
+const validateFirstName = () =>
+	firstName.value.match(/^[a-zA-Z]+$/)
+		? setValid(firstName)
+		: setInvalid(firstName)
+
+const validateLastName = () =>
+	lastName.value.match(/^[a-zA-Z\s]*$/)
+		? setValid(lastName)
+		: setInvalid(lastName)
+
+const validatePhone = () =>
+	phone.value.match(/^[1-9]\d{9}$/) ? setValid(phone) : setInvalid(phone)
+
+const validateEmail = () =>
+	email.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+		? setValid(email)
+		: setInvalid(email)
+
+const validateAgeGroup = () =>
+	ageGroup.value ? setValid(ageGroup) : setInvalid(ageGroup)
+
+const validateDesiredTeam = () => {
+	Array.from(desiredTeam).find((team) => team.checked)
+		? desiredTeam.forEach((team) => setValid(team))
+		: desiredTeam.forEach((team) => setInvalid(team))
+}
+
+const validateDesiredPosition = () => {
+	Array.from(desiredPosition).find((position) => position.checked)
+		? desiredPosition.forEach((position) => setValid(position))
+		: desiredPosition.forEach((position) => setInvalid(position))
+}
+
+const validatePincode = () =>
+	pincode.value.length === 0 || pincode.value.match(/^[1-9]\d{5}$/)
+		? setValid(pincode)
+		: setInvalid(pincode)
+
+const validateCountry = () =>
+	countries.value ? setValid(countries) : setInvalid(countries)
+
+const validateState = () =>
+	states.value ? setValid(states) : setInvalid(states)
+
+const validateCity = () =>
+	cities.value ? setValid(cities) : setInvalid(cities)
+
+const setValid = (node) => {
+	node.classList.replace('is-invalid', 'is-valid') &&
+		setTimeout(() => {
+			node.classList.remove('is-valid')
+		}, 3000)
+	return true
+}
+
+const setInvalid = (node) => {
+	node.classList.add('is-invalid')
+	return false
+}
+
+userName.addEventListener('input', () => {
+	// Some validation calls here
+})
+
+// Event listeners for validations
+firstName.addEventListener(
+	'input',
+	debounce(() => validateFirstName())
+)
+
+lastName.addEventListener(
+	'input',
+	debounce(() => validateLastName())
+)
+
+phone.addEventListener(
+	'input',
+	debounce(() => validatePhone())
+)
+
+email.addEventListener(
+	'input',
+	debounce(() => validateEmail())
+)
+
+ageGroup.addEventListener('change', validateAgeGroup)
+
+desiredTeam.forEach((team) =>
+	team.addEventListener('change', validateDesiredTeam)
+)
+
+desiredPosition.forEach((position) =>
+	position.addEventListener('change', validateDesiredPosition)
+)
+
+pincode.addEventListener(
+	'input',
+	debounce(() => validatePincode())
+)
+
+countries.addEventListener('change', validateCountry)
+
+states.addEventListener('change', validateState)
+
+cities.addEventListener('change', validateCity)
+
+// Event listener for form submission
+form.addEventListener('submit', (event) => {
+	event.preventDefault()
 })
